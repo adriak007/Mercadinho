8c3782c275f2bc607bebcfa4d76b6699d540d570
adriak007 <adriak007@gmail.com>
2025-12-08 20:02:14 -0300

    teste

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 };
+=======
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+>>>>>>> Stashed changes
 
 export default nextConfig;
