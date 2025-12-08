6b1ccd23362d1d9ac872eb6c4ff27a1b41b3793b
adriak007 <adriak007@gmail.com>
2025-12-08 20:27:53 -0300

    atuali

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,5 +1,10 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import type { NextConfig } from "next";
+=======
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+>>>>>>> Stashed changes
 
 const nextConfig: NextConfig = {
   /* config options here */
