98a60308e1d29a41868084a6858a4f946fe4c45e
adriak007 <adriak007@gmail.com>
2025-12-08 21:26:41 -0300

    versao atual

--- my-app/src/app/not-found.tsx
+++ my-app/src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFoundPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center p-6">
@@ -9,12 +11,13 @@ export default function NotFoundPage() {
         A página que você está procurando pode ter sido removida, ter mudado de
         endereço ou não existir.
       </p>
-      <link
+
+      <Link
         href="/"
         className="px-6 py-3 bg-blue-600 text-white rounded-sm shadow hover:bg-blue-700 transition"
       >
         Voltar para a Home
-      </link>
+      </Link>
     </div>
   );
 }
